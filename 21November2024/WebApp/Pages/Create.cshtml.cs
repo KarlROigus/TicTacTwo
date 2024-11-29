ac5788035ccb1cb990f73a0e35fad9abaa75580b
KarlROigus <karlrudolfoigus@gmail.com>
2024-11-29 11:55:12 +0200

    Login and creating user looks to be working

--- 21November2024/WebApp/Pages/Create.cshtml.cs
+++ 21November2024/WebApp/Pages/Create.cshtml.cs
@@ -1,11 +1,50 @@
+using DAL;
+using Domain;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace WebApp.Pages;
 
 public class Create : PageModel
 {
-    public void OnGet()
+
+    private readonly AppDbContext _db;
+    
+    public Create(AppDbContext dbContext)
     {
+        _db = dbContext;
+    }
+    
+    
+    [BindProperty]
+    public string UserName { get; set; } = default!;
+    
+    public string Error { get; set; } = default!;
+
+    
+
+    public IActionResult OnPost()
+    {
+        if (string.IsNullOrEmpty(UserName))
+        {
+            Error = "Cannot make an account with empty username!";
+            return Page();
+        }
         
+        var user = _db.Users.FirstOrDefault(each => each.Username == UserName);
+
+        if (user == null)
+        {
+            Console.WriteLine("HERE");
+            _db.Users.Add(new User()
+            {
+                Username = UserName
+            });
+            _db.SaveChanges();
+            return RedirectToPage("./Home", new { UserName = UserName });
+        }
+
+        Error = "The username already exists! Please try again!";
+        return Page();
     }
 }
--- 21November2024/WebApp/Pages/Login.cshtml.cs
+++ 21November2024/WebApp/Pages/Login.cshtml.cs
@@ -1,11 +1,43 @@
+using DAL;
+using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 
 namespace WebApp.Pages;
 
 public class Login : PageModel
 {
-    public void OnGet()
+
+    private readonly AppDbContext _db;
+    
+    public Login(AppDbContext dbContext)
     {
+        _db = dbContext;
+    }
+    
+
+    [BindProperty]
+    public string UserName { get; set; } = default!;
+
+    public string Error { get; set; } = default!;
+
+    
+
+    public IActionResult OnPost()
+    {
+        if (string.IsNullOrEmpty(UserName))
+        {
+            Error = "Cannot log in with empty username!";
+            return Page();
+        }
         
+        var user = _db.Users.FirstOrDefault(each => each.Username == UserName);
+
+        if (user != null)
+        {
+            return RedirectToPage("./Home", new { UserName = UserName});
+        }
+
+        Error = "No username found with the given name! Please try again!";
+        return Page();
     }
 }
